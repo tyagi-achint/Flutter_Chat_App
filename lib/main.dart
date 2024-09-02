08d056440e2fb99b5ccaa218ab58bba5e4ebde85
tyagi-achint <achinttyagi001@gmail.com>
2024-09-03 03:08:50 +0530

    working

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import 'components/firebase_options.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/chat_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: options);
+
   runApp(FamilyChat());
 }
 
